cf9d317feca8d5d92e89d83ed7b934f133c1e770
Michi1909 <michaela.kaiser1993@outlook.de>
2023-10-22 21:18:30 +0200

    add function berechneNoten

--- AuPi/Aufgaben2/Notenberechenung.java
+++ AuPi/Aufgaben2/Notenberechenung.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
+
 public class Notenberechenung {
 
     public static void main(String[] args ){
 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bitte die Punkteanzahl des Studenten eingeben!:");
+        double punkte = sc.nextDouble();
+        double note = berechneNoten(punkte);
+        System.out.println("Die Note ist hiermit: "+ note);
     }
 
     //TODO:
     public static double berechneNoten(double punkte){
-        double note = 0.0;
-        if(punkte<60){
-            
-        }
-        return note;
+            if (punkte >= 105) return 1.0;
+            else if (punkte >= 100) return 1.3;
+            else if (punkte >= 95) return 1.7;
+            else if (punkte >= 90) return 2.0;
+            else if (punkte >= 85) return 2.3;
+            else if (punkte >= 80) return 2.7;
+            else if (punkte >= 75) return 3.0;
+            else if (punkte >= 70) return 3.3;
+            else if (punkte >= 65) return 3.7;
+            else if (punkte >= 60) return 4.0;
+            else if (punkte >= 50) return 4.7;
+            else return 5.0;
     }
 }
