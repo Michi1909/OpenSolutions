8046af22ea7ce3e96b79ca79bf25db806ece515c
Michi1909 <michaela.kaiser1993@outlook.de>
2023-11-02 22:24:38 +0100

    changed SubtraktionUndAddition

--- AuPi/Aufgaben3/SubtraktionUndAddition.java
+++ AuPi/Aufgaben3/SubtraktionUndAddition.java
@@ -7,27 +7,13 @@ public class SubtraktionUndAddition {
         int b = sc.nextInt();
         int multiplikation = berechneMultiplikation(a, b);
         System.out.println("Die Berechnung der Multiplikation der beiden Ganzenzahlen a:"+a+" und b:"+b+" ergibt: "+ multiplikation);
-        //Validate if there is a number negative
-        if(ueberpruefeObNegativeZahl(a,b)){
-            //When number <0 enter here
-            int subtraktion = berechneSubtraktion(a,b);
-            System.out.println("Die Berechnung der Subtraktion der beiden Ganzenzahlen a:"+a+" und b:"+b+" ergibt: "+ subtraktion);
-        }else {
-            //When numbers !<0 enter here
-            int addition = berechneAddition(a, b);
-            System.out.println("Die Berechnung der Addition der beiden Ganzenzahlen a:" + a + " und b:" + b + " ergibt: " + addition);
-        }
+        int subtraktion = berechneSubtraktion(a,b);
+        System.out.println("Die Berechnung der Subtraktion der beiden Ganzenzahlen a:"+a+" und b:"+b+" ergibt: "+ subtraktion);
+        int addition = berechneAddition(a, b);
+        System.out.println("Die Berechnung der Addition der beiden Ganzenzahlen a:" + a + " und b:" + b + " ergibt: " + addition);
+
     }
 
-    /*
-    Method validate if there is a number negative
-     */
-    public static boolean ueberpruefeObNegativeZahl(int a, int b){
-        if((a<0 && b>=0)||(b<0 && a>=0)){
-            return true;
-        }
-        return false;
-    }
 
     /*
     Calculate multiplication of 2 numbers
@@ -40,6 +26,18 @@ public class SubtraktionUndAddition {
     Calculate multiplication when there is a number negative
      */
     public static int berechneSubtraktion(int a, int b){
+       return logicBehind(a,b);
+
+    }
+
+    /*
+    Calculate when there is no number negative
+     */
+    public static int berechneAddition(int a, int b){
+        return logicBehind(a,b);
+    }
+
+    public static int logicBehind(int a,int b){
         int c=0;
         if(a<0 && b>=0){
             for(int i=0;i<b;i++){
@@ -51,20 +49,14 @@ public class SubtraktionUndAddition {
                 c+=b;
             }
             return c;
+        }else if(a<0 && b<0){
+            for(int i=0;i<Math.abs(b);i++){
+                c-=(a);
+            }
+            return c;
+        }else{
+            return 0;
         }
-        return 0;
-
-    }
-
-    /*
-    Calculate when there is no number negative
-     */
-    public static int berechneAddition(int a, int b){
-        int c = a;
-        for(int i=0;i<b;i++){
-            c+=c;
-        }
-        return c;
     }
 
 }
