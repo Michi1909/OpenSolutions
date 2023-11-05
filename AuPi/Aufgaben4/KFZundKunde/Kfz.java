f33806b6126b201fd8331f2e2fea374a89cd2c1a
Michi1909 <michaela.kaiser1993@outlook.de>
2023-11-05 22:04:56 +0100

    add new Packages

--- AuPi/Aufgaben4/KFZundKunde/Kfz.java
+++ AuPi/Aufgaben4/KFZundKunde/Kfz.java
@@ -1,19 +1,41 @@
 package KFZundKunde;
 
 public class Kfz {
-    String kennzeichen;
-    String hersteller;
-    String modell;
-    public static void vermieten(){
-
+    String kennzeichen, modell;
+    int kmStand;
+    Kfz(String kennzeichen, String modell)
+    {
+        this.kennzeichen = kennzeichen;
+        this.modell = modell;
     }
-    public static void zuruecknehmen(){
-
+    Kfz(String kennzeichen, String modell, int kmStand)
+    {
+        this(kennzeichen, modell);
+        this.kmStand = kmStand;
     }
-    public static void beschaffen(){
-
+    void setKennzeichen(String kennzeichen)
+    {
+        this.kennzeichen = kennzeichen;
     }
-    public static void ausmustern(){
+    void setModell(String modell)
+    {
+        this.modell = modell;
+    }
+    void setKmStand(int kmStand)
+    {
+        this.kmStand = kmStand;
+    }
+    String getKennzeichen()
+    {
+        return this.kennzeichen;
+    }
+    String getModell()
+    {
+        return this.modell;
+    }
+    int getKmStand()
+    {
+        return this.kmStand;
 
     }
 
